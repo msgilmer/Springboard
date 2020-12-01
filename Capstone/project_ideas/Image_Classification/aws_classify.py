8038f0cd428b5e91dcbef1b6a07d0f862194e340
msgilmer <msgilmer@ncsu.edu>
2020-11-30 17:20:31 -0700

    Added new directory, deep_learning and inside a Jupyter notebook following the tutorial of 14.4.2 gilmer_predicting_fuel_efficiency_exercise.ipynb

--- Capstone/project_ideas/Image_Classification/aws_classify.py
+++ Capstone/project_ideas/Image_Classification/aws_classify.py
@@ -1,16 +1,16 @@
 import boto3
 
-#documentName = 'data/seg_pred/seg_pred/3.jpg'
 documentName = '3.jpg'
 bucketName = 'intelimages'
 
-client = boto3.client('rekognition', 'us-west-1', aws_access_key_id = 'AKIAQOSDAUHZKRALOVHY', aws_secret_access_key = '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')
+client = boto3.client('rekognition', 'us-west-1', aws_access_key_id = '', aws_secret_access_key = '')
 response = client.detect_labels(
         Image = {
             'S3Object': {'Bucket': bucketName, 'Name': documentName}
         },
         MaxLabels = 10,
-        MinConfidence = 60)
+        MinConfidence = 60
+)
 
 for elabel in response["Labels"]:
     print("Label: {}, Confidence: {}%".format(elabel["Name"], elabel["Confidence"]))
